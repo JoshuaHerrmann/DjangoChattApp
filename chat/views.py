8ffec0c920e894ee61881b67555e0f25f89060b5
Joshua Herrmann <joshua.herrmann1@web.de>
2022-09-17 18:25:28 +0200

    added chat route

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request): 
+    return render(request, 'chat/index.html')           # templates/chat/index.html steht da quasi, da die views.py immer in nach templates folder sucht
+    
+
+
